f7e56d77f4756fd871fd8cae3f81956b57b074ca
anshagarwal1234 <102647935+anshagarwal1234@users.noreply.github.com>
2024-04-16 21:43:16 +0530

    Add files via upload

---
+++ server.py
@@ -0,0 +1,37 @@
+import socket
+from  threading import Thread
+
+IP_ADDRESS = '127.0.0.1'
+PORT = 8050
+SERVER = None
+BUFFER_SIZE=4096
+clients = {}
+
+
+
+def setup():
+    
+    print("\n\t\t\t\t\t\tIP MESSENGER\n") 
+    
+    #Getting global values
+
+    global PORT
+    global IP_ADDRESS
+    global SERVER
+
+    
+    SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    SERVER.bind((IP_ADDRESS, PORT))
+
+    #Listening incomming connections SERVER.listen (100)
+    print("\t\t\t\tSERVER IS WAITING FOR INCOMMING CONNECTIONS...") 
+    print("\n")
+    
+
+   
+
+
+setup_thread = Thread(target=setup)           #receiving multiple messages
+setup_thread.start()
+
+# ------ Bolierplate Code End -----------
